7ff8e65d36faaa214a831093f36f24963184ca7b
Rachel King <rachel.b.king@gmail.com>
2014-01-31 17:04:07 -0800

    Added hallway rooms and starter items.

--- library.py
+++ library.py
@@ -18,15 +18,16 @@ class Player(object):
         self.shape = 'human'
         self.size = 'medium'
         self.flying = False
-        self.known_spells = ['human', 'otter']
+        self.known_spells = ['human']
         self.inventory = []
 
     def inventory_check(self):
         if self.inventory == []: print "You're not holding anything!"
         else:
             print "You're holding:"
+            print
             for thing in self.inventory:
-                print "\n%s" % (thing)
+                print "%s" % (thing)
 
     def spell_check(self):
         if self.inventory == ['human']: print "You don't know any spells."
@@ -40,12 +41,13 @@ class Player(object):
         if direction == 'e' and self.location == rooms.reading_room:
             print "No, I really don't think you want to go that way. Why don't you stick to the library?"
         elif direction not in self.location.directions:
-            print "There's a wall there, dummy."
+            print "You can't go that way, stupid."
         elif direction in self.location.directions and self.location.directions[direction].locked == True:
             print "That door's locked. And it'll stay locked no matter how many times you tug on the handle, so stop trying."
         # elif direction in rooms.self.location.directions and...:
         #     print "That opening is too small for a full-sized person. Perhaps something smaller, like a cat or otter, could get through."
-        #need a way to ID a DOOR (as opposed to a room, which I did for the locked rooms above), since a door goes both ways and a key is one-time.
+        #need a way to ID a DOOR (as opposed to a room, which I did for the locked rooms above),
+        #since a door goes both ways and a key is one-time in one direction.
         elif direction in self.location.directions:
             self.location = self.location.directions[direction]
             self.location.describe()
@@ -61,7 +63,7 @@ class Item(object):
         item_list[name] = self
 
     def examine(self):
-        if self.location == player.location:
+        if self.location == player.location or self.location == 'player':
             print self.description
         else: print "I'm sorry, I don't see that item."
 
@@ -191,25 +193,41 @@ spells you know), "exit" or "quit" (exits the game), or "restart" (restarts the 
 
     def command(self, user_input):
         verb = user_input[0]
+        noun = ''
         if len(user_input) == 2: noun = user_input[1]
         elif len(user_input) > 2: print "Whoops! That's too challenging for me. Please try again."
 
         verbs = {'help': self.help_command, 'exit': sys.exit, 'quit': sys.exit, 'restart': self.restart,
         'look': player.location.describe, 'inventory': player.inventory_check, 'i': player.inventory_check,
         'save': self.save, 'load': self.load, 'spells': player.spell_check}
+        # 'examine': item_list[noun].examine, 'take': item_list[noun].take, 'drop': item_list[noun].drop}
 
         #the following verbs entries are within a try because they require noun to be defined.
-        try:
-            verbs['examine'] = item_list[noun].examine
-            verbs['take'] = item_list[noun].take
-            verbs['drop'] = item_list[noun].drop
-        except: pass
+        # try:
+        #     verbs['examine'] = item_list[noun].examine
+        #     verbs['take'] = item_list[noun].take
+        #     verbs['drop'] = item_list[noun].drop
+        # except: pass
 
+        #can't *try* here because sys.exit() doesn't work within a try.
         if verb in verbs:
             verbs[verb]()
-        #can't *try* here because sys.exit() doesn't work within a try.
         else:
             if verb == 'hello' or verb == 'hi': print "Hullo!"
+            elif verb == 'examine':
+                item_list[noun].examine()
+            elif verb == 'take':
+                if noun == 'all':
+                    for item in player.location.inventory:
+                        item_list[item].take()
+                else:
+                    item_list[noun].take()
+            elif verb == 'drop':
+                if noun == 'all':
+                    for item in player.location.inventory:
+                        item_list[item].drop()
+                else:
+                    item_list[noun].drop()
             elif verb in moves: player.move(moves[verb])
             elif verb == 'read' or verb == 'open':
                 try: item_list[noun].open()
@@ -231,11 +249,18 @@ pan_pipes = Item('pipes', '''It's a set of pan pipes. There are seven total. The
 bound together with leather, and inscribed with Charter marks.''')
 bells = Item('bells', '''A set of seven bells hang on a bandolier, meant to be worn across the chest. Their leather pouches are
 etched with Charter marks and the bells' mahogany handles stick out of the top of the pouches.''')
+mouse = Item('mouse', '''It's a small clockwork mouse, about the size of your fist. There's a turnkey sticking out of the back.
+Interestingly, it also buzzes in your hand, like it's connected to the Charter somehow.''', rooms.third_assistant_study)
+key = Item('key', '''It's a silver bracelet, set with seven emeralds. One is glowing.''', rooms.third_assistant_study)
+waistcoat = Item('waistcoat', '''A yellow waistcoat. It flatters you quite nicely. And so professional!
+There's a whistle clipped to the lapel.''', rooms.third_assistant_study)
+dagger = Item('dagger', '''A silver dagger in a leather scabbard, attached to a belt. The blade looks quite sharp
+and there are Charter marks etched on the hilt.''', rooms.third_assistant_study)
 
 #Books (Subclass of Items)
-ledger = Book('''As you expected, the book is full of lists of equipment checked out to past librarians. And there's
-your name, at the very bottom! There's a list of equipment for you, including a dagger, a clockwork mouse, a key, and a yellow
-waistcoat. Now, I wonder where those things could be. (You didn't lose them already, did you?)''', None, 'ledger',
+ledger = Book('''As you expected, the book is full of lists of equipment checked out to past librarians. And there's your name,
+at the very bottom! There's a list of equipment for you, including a dagger, a clockwork mouse, a key, and
+a yellow waistcoat. Now, I wonder where those things could be. (You didn't lose them already, did you?)''', None, 'ledger',
 '''It's a large leather ledger. It's incredibly heavy, and when you open it you feel as though it
 contains every piece of equipment checked out by every librarian in the history of the Clayr. It's that big. You probably don't want
 to carry it around.''', rooms.librarian_alcove)
@@ -275,3 +300,4 @@ game.start()
 #moved from their original location.
 #SOLVED: #Taking an item puts it in your inventory and removes it from the room, but doesn't change the room description.
 #Bug: moving not working since dividing into multiple files. The "self" part of the location is confusing it.
+#Bug: 'take all' only takes the first two items. Trying 'take all' again then only takes the next one item (in a room with four items).
--- rooms.py
+++ rooms.py
@@ -1,3 +1,5 @@
+directory = []
+
 class Room(object):
     def __init__(self, name, description, locked=False):
         self.name = name
@@ -5,6 +7,7 @@ class Room(object):
         self.directions = {}
         self.inventory = []
         self.locked = locked
+        directory.append(self)
 
     def add_directions(self, directions):
         self.directions = directions
@@ -15,28 +18,32 @@ class Room(object):
         print self.description
         if len(self.inventory) == 1:
             print "\nThere's a %s here." % (self.inventory[0])
-        elif len(self.inventory) > 1:
+        elif len(self.inventory) == 2:
             print "\nThere are a %s and %s here." % (self.inventory[0], self.inventory[1])
-            #how to make this flexible for more than two items in the room?
+        #how to make this flexible, no matter how many items in the room?
+        elif len(self.inventory) == 3:
+            print '\nThere are a %s, %s, and %s here.' % (self.inventory[0], self.inventory[1], self.inventory[2])
+        elif len(self.inventory) == 4:
+            print '\nThere are a %s, %s, %s, and %s here.' % (self.inventory[0], self.inventory[1], self.inventory[2], self.inventory[3])
 
 #Rooms Inits
 reading_room = Room("Reading Room", '''You're in the Main Reading Room. Large wooden tables fill the room. Clayr sit
 at some of the tables, reading. There are exits to the south and west. The main doors are open to the east.''')
-librarian_alcove = Room("Librarian Alcove", '''This is the librarian alcove, the main hub of their behind-the-scenes
-library management. There are exits to the north, south, east, and west. There is a small roller-top desk in the corner.''')
+librarian_alcove = Room("Librarian Alcove", '''This is the librarian alcove, the main hub of their behind-the-scenes library management.
+There are exits to the north, south, east, and west. There is a small roller-top desk in the corner.''')
 #possibly open the desk?
 binding_room = Room("Binding Room", '''This is the room where the librarians repair damaged books. There are books covering
 every flat surface, and a giant press in the back corner. The only exit is to the east.''')
 robing_room = Room("Robing Room", '''You're in a room full of miscellaneous useful things. Boat hooks, climbing ropes,
 and weapons line the walls. The only exit is to the west.''')
-upper_librarian_hallway = Room("Hallway", '''You're standing in a hallway, in the private librarians' wing of the library. Painted blue doors
-line the hallway, but they're all closed and locked. There's another door at the south end of the hallway. There's an exit to the north, and
-steps leading down.''')
+upper_librarian_hallway = Room("Hallway", '''You're standing in a hallway, in the employees-only librarians' wing of the library. Painted blue doors
+line the hallway, but they're all closed and locked. There's another door at the south end of the hallway.
+There's an exit to the north, and steps leading down.''')
 chiefs_office = Room("Chief Librarian Vancelle's Office", '''This is Chief Librarian Vancelle's office. It's a roomy, wood-panelled office.
 Chief Librarian Vancelle is obviously not the tidiest person; papers and books are stacked willy-nilly on her desk. She's nice
 enough, but I wouldn't like to get caught in her office without her permission. The only exit is to the north.''', True)
-middle_librarian_hallway = Room("Hallway", '''You're standing in a hallway. All of the doors are painted red. They're all closed and locked,
-except one. The far door on the east side is open. There are stairs leading up and down.''')
+middle_librarian_hallway = Room("Hallway", '''You're standing in a hallway. All of the doors are painted red. They're all closed and locked.
+There are stairs leading up and down.''')
 second_assistant_study = Room("Second Assistant Study", '''This is your new study, the room of a Second Assistant Librarian. There's enough
 room for a desk and not one but two chairs (what luxury!), and there's a door ajar that leads to a tiny bathroom, all your own. The only exit
 is to the west. ''', True)
@@ -46,8 +53,46 @@ upper floors. The doors are all painted yellow. They're all closed, except for t
 third_assistant_study = Room("Third Assistant Study", '''This is your study. It's very cramped; there's barely room for the desk and
 single chair that are here. The only exit is to the west.''')
 
+hall1 = Room("Hallway", '''You're in a hallway. There are archways to the east and west.
+Through the eastern archway, you can see the Main Reading Room.''')
+hall2 = Room("Hallway", '''You're in a hallway. There are archways to the east and west.''')
+hall3 = Room("Hallway", '''You're in a hallway. There are archways to the east and south.''')
+
+hall4 = Room("Hallway", '''You're in a hallway. There are archways to the north and south.''')
+hall5 = Room("Hallway", '''You're in a hallway. There are archways to the north and south.''')
+hall6 = Room("Hallway", '''You're in a hallway. There are archways to the north and east.''')
+
+hall7 = Room("Hallway", '''You're in a hallway. There are archways to the east and west.''')
+hall8 = Room("Hallway", '''You're in a hallway. There are archways to the east and west.''')
+hall9 = Room("Hallway", '''You're in a hallway. There are archways to the north and west.''')
+
+hall10 = Room("Hallway", '''You're in a hallway. There are archways to the north and south.''')
+hall11 = Room("Hallway", '''You're in a hallway. There are archways to the north and south.''')
+hall12 = Room("Hallway", '''You're in a hallway. There are archways to the west and south.''')
+
+hall13 = Room("Hallway", '''You're in a hallway. There are archways to the east and west.''')
+hall14 = Room("Hallway", '''You're in a hallway. There are archways to the east and south.''')
+
+hall15 = Room("Hallway", '''You're in a hallway. There are archways to the north and south.''')
+hall16 = Room("Hallway", '''You're in a hallway. There are archways to the north and east.''')
+
+hall17 = Room("Hallway", '''You're in a hallway. There are archways to the east and west.''')
+hall18 = Room("Hallway", '''You're in a hallway. There are archways to the north and west.''')
+
+hall19 = Room("Hallway", '''You're in a hallway. There are archways to the north and south.''')
+hall20 = Room("Hallway", '''You're in a hallway. There are archways to the west and south.''')
+
+hall21 = Room("Hallway", '''You're in a hallway. There are archways to the east and south.''')
+
+hall22 = Room("Hallway", '''You're in a hallway. There are archways to the north and east.''')
+
+hall23 = Room("Hallway", '''You're in a hallway. There are archways to the north and west.''')
+
+hall24 = Room("Hallway", '''You're in a hallway. There's an archway to the south.
+On the west wall, there's a hole, far too small for a human to pass through.''')
+
 #Room Directions
-reading_room.add_directions({'s': librarian_alcove})
+reading_room.add_directions({'s': librarian_alcove, 'w': hall1})
 #I need a "You don't really want to go that way" for when people try to go east.
 librarian_alcove.add_directions({'n': reading_room, 'w': binding_room, 'e': robing_room, 's': upper_librarian_hallway})
 binding_room.add_directions({'e': librarian_alcove})
@@ -58,4 +103,28 @@ second_assistant_study.add_directions({'w': middle_librarian_hallway})
 lower_librarian_hallway.add_directions({'u': middle_librarian_hallway, 'e': third_assistant_study})
 third_assistant_study.add_directions({'w': lower_librarian_hallway})
 
-directory = [reading_room, librarian_alcove, binding_room, robing_room, upper_librarian_hallway, middle_librarian_hallway, lower_librarian_hallway, chiefs_office]
+hall1.add_directions({'e': reading_room, 'w': hall2})
+hall2.add_directions({'e': hall1, 'w': hall3})
+hall3.add_directions({'e': hall2, 's': hall4})
+hall4.add_directions({'n': hall3, 's': hall5})
+hall5.add_directions({'n': hall4, 's': hall6})
+hall6.add_directions({'e': hall7, 'n': hall5})
+hall7.add_directions({'e': hall8, 'w': hall6})
+hall8.add_directions({'e': hall9, 'w': hall7})
+hall9.add_directions({'n': hall10, 'w': hall8})
+hall10.add_directions({'n': hall11, 's': hall9})
+hall11.add_directions({'n': hall12, 's': hall10})
+hall12.add_directions({'w': hall13, 's': hall11})
+hall13.add_directions({'e': hall12, 'w': hall14})
+hall14.add_directions({'e': hall13, 's': hall15})
+hall15.add_directions({'n': hall14, 's': hall16})
+hall16.add_directions({'n': hall15, 'e': hall17})
+hall17.add_directions({'e': hall18, 'w': hall16})
+hall18.add_directions({'n': hall19, 'w': hall17})
+hall19.add_directions({'n': hall20, 's': hall18})
+hall20.add_directions({'w': hall21, 's': hall19})
+hall21.add_directions({'e': hall20, 's': hall22})
+hall22.add_directions({'e': hall23, 'n': hall21})
+hall23.add_directions({'w': hall22, 'n': hall24})
+hall24.add_directions({'s': hall23})
+
